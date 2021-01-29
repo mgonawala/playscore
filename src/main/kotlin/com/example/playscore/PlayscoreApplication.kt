bee2ab1b8822a28dfc7765fe967f867748511d01
mgonawala <mohini.gonawala90@gmail.com>
2021-01-29 18:16:45 +0530

    Initial Commit

---
+++ src/main/kotlin/com/example/playscore/PlayscoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.playscore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayscoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlayscoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/playscore/PlayscoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.playscore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlayscoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
